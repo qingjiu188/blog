d4833e8684ec540cf067b2963858a8c0c249baba
qingjiu8 <qingjiu4@qq.com>
2025-08-18 13:59:10 +0800

    条件渲染（苦难版）

--- React/25-08-18-01/react-basic/src/App.js
+++ React/25-08-18-01/react-basic/src/App.js
@@ -1,18 +1,25 @@
-//项目的根组件
-// App.js——>index.js->public/index.html(id=root)
+//定义文章类型
 
-const isLogin = false
+const articleType = 1   //0,1,3
+
+//定义核心函数（根据文章类型返回不同的JSX模板）
+
+function getArticleTem() {
+  if (articleType === 0) {
+    return <div>文章类型为0的模板</div>;
+  } else if (articleType === 1) {
+    return <div>文章类型为1的模板</div>;
+  } else {
+    return <div>文章类型为3的模板</div>;
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      {/* 逻辑与&& */}
-      {isLogin && <span>this is span</span>}
-      {/* 三元运算 */}
-      <p>
-      {isLogin ? <span>this is true </span> : <span>this is false</span>}
-      </p>
-      </div>
+      {/* 调用函数渲染不同的模板 */}
+      {getArticleTem()}
+    </div>
   );
 }
 
