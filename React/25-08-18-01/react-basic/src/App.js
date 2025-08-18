44d0cf049c83a34fd9a64e98246b68297108a458
qingjiu8 <qingjiu4@qq.com>
2025-08-18 13:16:49 +0800

    渲染列表

--- React/25-08-18-01/react-basic/src/App.js
+++ React/25-08-18-01/react-basic/src/App.js
@@ -1,27 +1,19 @@
 //项目的根组件
 // App.js——>index.js->public/index.html(id=root)
 
-const count =100
-
-function getName() {
-  return '张三';
-}
+const list =[
+  {id: 1, name: 'List1'},
+  {id: 2, name: 'List2'},
+  {id: 3, name: 'List3'}
+];
 function App() {
   return (
     <div className="App">
       this is App
-      {/* 使用引号传递字符串 */}
-      {'这是字符串'}
-      {/* 识别js变量 */}
-      {count}
-      {/* 函数调用 */}
-      {getName()}
-      {/* 方法调用 */}
-      {new Date().getDate()}
-      {/* 使用js对象 */}
-      <div style={{ color: 'red', fontSize: '20px' }}>
-        这是一个div
-        </div>
+      {/* 渲染列表 */}
+      <ul>
+        {list.map(item=><li key={item.id}>{item.name}</li>)}
+      </ul>
       </div>
   );
 }
