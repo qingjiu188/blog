883f2cbc7460a6e07226f554e37a8c5cff461f33
qingjiu8 <qingjiu4@qq.com>
2025-08-18 16:51:53 +0800

    状态变量（状态不可变）

--- React/25-08-18-01/react-basic/src/App.js
+++ React/25-08-18-01/react-basic/src/App.js
@@ -1,24 +1,19 @@
-//useState实现一个计数器
-
 import { useState } from "react";
 
-
 function App() {
-// 调用useState添加一个状态变量
-//count 状态变量
-// setCount改变状态变量的方法
-  const [count,setCount] = useState(0)
+  let [count,setCount] = useState(0)
 
-  //点击事件回调
-  const handleClick = () => {
-    //作用1:用传入的新值修改count
-    //2:重新使用新的count渲染ui
+  const handldClick = () => {
+
+    //直接修改不能引发视图更新
+    // count++
+    // console.log(count)
+
     setCount(count+1)
   }
-
-  return (
+    return (
     <div className="App">
-      <button onClick={handleClick}>{count}</button>
+      <button onClick={handldClick}>{count}</button>
     </div>
   );
 }
