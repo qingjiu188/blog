22084f38492cbed3f6e1b07de42243e72b681e75
qingjiu8 <qingjiu4@qq.com>
2025-08-18 13:48:39 +0800

    条件渲染（简单情况）

--- React/25-08-18-01/react-basic/src/App.js
+++ React/25-08-18-01/react-basic/src/App.js
@@ -1,23 +1,17 @@
 //项目的根组件
 // App.js——>index.js->public/index.html(id=root)
 
-const list =[
-  {id: 1, name: 'List1'},
-  {id: 2, name: 'List2'},
-  {id: 3, name: 'List3'}
-];
+const isLogin = false
+
 function App() {
   return (
     <div className="App">
-      this is App
-      {/* 渲染列表 */}
-      {/* map循环哪个结构  return结构 */}
-      {/* 注意事项：加上一个独一无二的key属性  字符串或者数字也行   */}
-      {/* 在常规中，通常用id作为key来绑定 */}
-      {/* key的作用：React框架内部使用，提升更新性能 */}
-      <ul>
-        {list.map(item=><li key={item.id}>{item.name}</li>)}
-      </ul>
+      {/* 逻辑与&& */}
+      {isLogin && <span>this is span</span>}
+      {/* 三元运算 */}
+      <p>
+      {isLogin ? <span>this is true </span> : <span>this is false</span>}
+      </p>
       </div>
   );
 }
