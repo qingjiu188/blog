2e65a6c34be18c31764276c7a64a7e503946603f
qingjiu8 <qingjiu4@qq.com>
2025-08-18 15:28:51 +0800

    状态变量

--- React/25-08-18-01/react-basic/src/App.js
+++ React/25-08-18-01/react-basic/src/App.js
@@ -1,42 +1,25 @@
-// // 定义组件
-// function Button() {
-//   //组件逻辑
-//   return <button>click me!</button>
-// }
+//useState实现一个计数器
 
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* 使用组件 */}
-//       {/* 自闭和 */}
-//       <Button />
-//       {/* 成对 */}
-//       <Button></Button>
-//     </div>
-//   );
-// }
-
-
-//箭头函数的写法
-// 定义组件
-const Button = () => {
-  //组件逻辑
-  return <button>click me!</button>
-}
+import { useState } from "react";
 
 
 function App() {
+// 调用useState添加一个状态变量
+//count 状态变量
+// setCount改变状态变量的方法
+  const [count,setCount] = useState(0)
+
+  //点击事件回调
+  const handleClick = () => {
+    //作用1:用传入的新值修改count
+    //2:重新使用新的count渲染ui
+    setCount(count+1)
+  }
+
   return (
     <div className="App">
-      {/* 使用组件 */}
-      {/* 自闭和 */}
-      <Button />
-      {/* 成对 */}
-      <Button></Button>
+      <button onClick={handleClick}>{count}</button>
     </div>
   );
 }
-
-
 export default App;
