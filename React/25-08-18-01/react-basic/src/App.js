78446cf5231455a382b14b12400f57a0cca37ace
qingjiu8 <qingjiu4@qq.com>
2025-08-20 16:43:36 +0800

    hook使用规则

--- React/25-08-18-01/react-basic/src/App.js
+++ React/25-08-18-01/react-basic/src/App.js
@@ -1,35 +1,22 @@
-//封装自定义Hook
+//hook使用规则
+//1.只能在组件中或者其他自定义hook函数中调用（不能在组件中使用）
+//2.只能在组件的顶层调用，不能嵌套在if，for，其他函数中
 
-//问题：布尔切换的逻辑 当前组件耦合在一起的 不方便复用
-//解决思路：自定义hook
+
+//演示1
+//在组件外使用
 import { useState } from "react"
+// useState('')
 
-function useToggle () {
-  //可复用的逻辑代码
-  const [value,setValue] = useState(true)
-
-  const toggle = () => setValue(!value)
-
-  //哪些状态和回调函数需要在其他组件中使用 用return
-  return {
-    value,
-    toggle
-  }
-}
-
-//封装自定义hook通用思路
-//1.声明一个以use开头的函数
-//2.在函数体内封装可复用的逻辑（只要是可复用的逻辑）
-//3.把组件中用到的状态或者回调 return出去（以对象或者数组）
-//4.在哪个组件中要用到这个逻辑，就执行这个函数，解构出来状态或者回调进行使用
-
+//演示2
+//在if for 组件内使用
 function App () {
-  const {value,toggle} = useToggle()
-
+  // if(Math.random() > 0.5) {
+  //   useState()
+  // }
   return (
     <div>
-      {value && <div>this is div</div>}
-      <button onClick={toggle}>toggle</button>
+      this is App
     </div>
   )
 }
