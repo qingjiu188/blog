81babc8641b30f185df75debc9ba038c7709cf61
qingjiu8 <qingjiu4@qq.com>
2025-08-21 14:39:52 +0800

    传参

--- React/redux/react-redux-pro/src/App.js
+++ React/redux/react-redux-pro/src/App.js
@@ -1,6 +1,6 @@
 import { useDispatch,useSelector } from "react-redux";
 //导入actionCreater
-import {inscrement,decrement} from './store/modules/counterStore'
+import {inscrement,decrement,addToNum} from './store/modules/counterStore'
 
 function App() {
   const { count } = useSelector(state =>state.counter)
@@ -10,6 +10,8 @@ function App() {
      <button onClick={() => dispatch(decrement())}>-</button>
       {count}
      <button onClick={() => dispatch(inscrement())} >+</button>
+     <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
+     <button onClick={() => dispatch(addToNum(200))}>add to 200</button>
     </div>
   );
 }
