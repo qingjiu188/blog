e1e84b866e8afc1e3ff59f1188992856bb2cac5f
qingjiu8 <qingjiu4@qq.com>
2025-08-21 16:40:43 +0800

    异步状态操作

--- React/redux/react-redux-pro/src/App.js
+++ React/redux/react-redux-pro/src/App.js
@@ -1,10 +1,18 @@
 import { useDispatch,useSelector } from "react-redux";
 //导入actionCreater
 import {inscrement,decrement,addToNum} from './store/modules/counterStore'
+import { useEffect } from "react";
+import {fetchChannlList} from './store/modules/channelStore'
 
 function App() {
   const { count } = useSelector(state =>state.counter)
+  const {channelList} =useSelector(state => state.channel)
   const dispatch = useDispatch()
+
+  //使用useEffect触发异步请求执行
+  useEffect(() => {
+    dispatch(fetchChannlList())
+  },[dispatch])
   return (
     <div className="App">
      <button onClick={() => dispatch(decrement())}>-</button>
@@ -12,6 +20,9 @@ function App() {
      <button onClick={() => dispatch(inscrement())} >+</button>
      <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
      <button onClick={() => dispatch(addToNum(200))}>add to 200</button>
+     <ul>
+      {channelList.map(item => <li key={item.id}>{item.name}</li>)}
+     </ul>
     </div>
   );
 }
