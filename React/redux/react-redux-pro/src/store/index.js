7fdbbfc91c1feaad668f1b7c380f3d4931a18527
qingjiu8 <qingjiu4@qq.com>
2025-08-21 14:11:24 +0800

    redux与react-实现counter

---
+++ React/redux/react-redux-pro/src/store/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+//导入子模块
+import counterReducer from './modules/counterStore'
+
+const store = configureStore({
+    reducer:{
+        counter:counterReducer
+    }
+})
+
+export default store
